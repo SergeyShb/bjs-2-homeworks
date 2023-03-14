86ddaec9179936c6de0c54c09c5d1ffc488545b4
Ser <Shabaykin.S@gmail.com>
2023-03-14 11:07:17 +0300

    Третье задание полностью выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) {
+        return true;
+      } else return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(item => item.gender === gender).map(item => item.age).reduce((acc, item, index, arr) => {
+    acc = acc + item; 
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc; 
+  }, 0);
+ 
+  return result;
 }
